e09d33a808f64b4633356ab043dde81e5934ec62
t7bo <tibo7w@gmail.com>
2023-11-23 14:03:51 +0100

    début du nettoyage dans pipelines

--- pokemons_scraper/pokemons_scraper/items.py
+++ pokemons_scraper/pokemons_scraper/items.py
@@ -4,9 +4,29 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import re
 
 
 class PokemonsScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+
+# def enlever_str(string):
+#     chiffres = re.findall(r'\d', string)
+#     return int(chiffres)
+
+
+class PokemonItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    description = scrapy.Field()
+    stock = scrapy.Field(stock_sans_str = enlever_str)
+    sku = scrapy.Field()
+    categories = scrapy.Field()
+    tags = scrapy.Field()
+    weight = scrapy.Field()
+    length = scrapy.Field()
+    width = scrapy.Field()
+    height = scrapy.Field()
--- pokemons_scraper/pokemons_scraper/pipelines.py
+++ pokemons_scraper/pokemons_scraper/pipelines.py
@@ -10,4 +10,31 @@ from itemadapter import ItemAdapter
 
 class PokemonsScraperPipeline:
     def process_item(self, item, spider):
+        
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+        
+        for field_name in field_names:
+            if field_name == 'price':
+                value = adapter.get(field_name)
+                adapter[field_name] = float(value)
+                
+            if field_name == 'sku':
+                sku_value = adapter.get("sku")
+                adapter[field_name] = int(sku_value)
+                
+            if field_name == 'stock':
+                stock_string = adapter.get("stock")
+                # split_string = stock_string.split('')
+                for character in stock_string:
+                    if character.isdigit() == False:
+                        stock_string.remove(character)
+                adapter[field_name] = int(stock_string)
+            
+            if field_name == 'tags':
+                tags_values = adapter.get("tags")
+                for tag in tags_values:
+                    tag = tag.title()
+                adapter[field_name] = tag
+            
         return item
